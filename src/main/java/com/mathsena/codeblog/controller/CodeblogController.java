418a35645d825cd3746948fb7bfc85daa037d8f6
MathSena <MathSena07@hotmail.com>
2023-04-24 22:58:23 -0300

    Inserindo mensagem de validação no Form e SummerNote para formatação

--- src/main/java/com/mathsena/codeblog/controller/CodeblogController.java
+++ src/main/java/com/mathsena/codeblog/controller/CodeblogController.java
@@ -3,12 +3,17 @@ package com.mathsena.codeblog.controller;
 import com.mathsena.codeblog.model.Post;
 import com.mathsena.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -31,6 +36,23 @@ public class CodeblogController {
         Post post = codeblogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value="/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @RequestMapping(value="/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
     }
 
 
