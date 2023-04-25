5c487e9026d2e12c9b7ef4492486790b5e74e690
MathSena <MathSena07@hotmail.com>
2023-04-24 22:18:39 -0300

    Criando um método GET com @PathVariable id

--- src/main/java/com/mathsena/codeblog/controller/CodeblogController.java
+++ src/main/java/com/mathsena/codeblog/controller/CodeblogController.java
@@ -4,7 +4,7 @@ import com.mathsena.codeblog.model.Post;
 import com.mathsena.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +17,21 @@ public class CodeblogController {
     @Autowired
     private CodeblogService codeblogService;
 
-    @RequestMapping(value="posts", method = RequestMethod.GET)
+    @RequestMapping(value="/posts", method = RequestMethod.GET)
     public ModelAndView getPosts(){
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
         return mv;
     }
+
+    @RequestMapping(value="/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("posts", post);
+        return mv;
+    }
+
+
 }
